d9ff43331e5c57594d87a88bde9849cc115be1e3
hryggrbyr <113116722+hryggrbyr@users.noreply.github.com>
2025-10-13 15:44:49 +0100

    Update src.11tydata.js

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -3,11 +3,17 @@ const slugify = require("slugify");
 module.exports = {
   eleventyComputed: {
     permalink: (data) => {
-      const pathParts = data.page.filePathStem.split("/");
+      const pathParts = data.page.filePathStem
+        .split("/")
+        .filter(part => part && part !== 'index');
+      
       const slugifiedParts = pathParts.map((part) =>
-        slugify(part, { lower: true, strict: true }),
+        slugify(part, { lower: true, strict: true })
       );
-      return `${slugifiedParts.join("/")}/index.html`;
+      
+      return slugifiedParts.length > 0
+        ? `/${slugifiedParts.join("/")}/index.html`
+        : `/index.html`;
     },
   },
 };
